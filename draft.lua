ae2327e97752d883a6079dd74b6046e30bc1e46d
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2015-08-18 20:52:12 -0400

    fixed a bug with triangles

--- draft.lua
+++ draft.lua
@@ -95,7 +95,7 @@ function draft:triangleIsosceles(cx, cy, width, height, mode)
 	local x3 = cx - widthRadius
 	local y3 = y2
 	if mode then
-		love.graphics.polygon(mode, x1, y1, x2, y2, x2, y3)
+		love.graphics.polygon(mode, x1, y1, x2, y2, x3, y3)
 	end
 	return {x1, y1, x2, y2, x3, y3}
 end
@@ -121,7 +121,7 @@ function draft:triangleRight(cx, cy, width, height, mode)
 	local x3 = x1
 	local y3 = y2
 	if mode then
-		love.graphics.polygon(mode, x1, y1, x2, y2, x2, y3)
+		love.graphics.polygon(mode, x1, y1, x2, y2, x3, y3)
 	end
 	return {x1, y1, x2, y2, x3, y3}
 end
