8f910d392d6b3263ce45242497dc357456fe54c1
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2015-09-21 15:19:16 -0400

    removed version number

--- draft.lua
+++ draft.lua
@@ -1,11 +1,9 @@
 --[[
 Title:     draft
 Use:       drafting module for the LÖVE 2D game engine
-Version:   1.1
 Author:    Pierre-Emmanuel Lévesque
 Email:     pierre.e.levesque@gmail.com
 Created:   July 26th, 2012
-Updated:   August 16th, 2015
 Copyright: Copyright 2012, Pierre-Emmanuel Lévesque
 License:   MIT license - @see LICENSE.md
 --]]
